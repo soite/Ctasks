9efb56a07efa43bfce55af48506f4440b2832b29
soite <mrxtrt416@gmail.com>
2025-05-15 22:16:42 +0330

    second

--- first.c
+++ first.c
@@ -59,9 +59,61 @@ void view_task() {
    printf("------------------\n");
 }
 
+void mark_done() {
+    int num;
+    view_task();
+    printf("Enter task number to mark as done: ");
+    scanf("%d", &num);
+    if(num < 1 || num > task_cnt) {
+       printf("Your input number is invalid\n");
+       return;
+    }
+    tasks[num - 1].done = 1;
+    save_task();
+    printf("Your task marked as done! \n");
+}
 
+void delete_task() {
+    int num;
+    view_task();
+    printf("Enter task number to delete: ");
+    scanf("%d", &num);
+    if(num < 1 || num > task_cnt) {
+       printf("Your input number is invalid\n");
+       return;
+    }
+    for(int i = num - 1; i < task_cnt - 1; i++) {
+       tasks[i] = tasks[i + 1];
+    }
+    task_cnt--;
+    save_task();
+    printf("Your task is deleted!\n");
+}
 
 
 int main() {
+    int choice;
+    load_task();
+    do {
+        printf("\nTo-Do List Manager\n");
+        printf("1. View Tasks\n");
+        printf("2. Add Task\n");
+        printf("3. Mark Task as Done\n");
+        printf("4. Delete Task\n");
+        printf("5. Exit\n");
+        printf("Choose an option: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1: view_task(); break;
+            case 2: add_task(); break;
+            case 3: mark_done(); break;
+            case 4: delete_task(); break;
+            case 5: printf("Goodbye!\n"); break;
+            default: printf("Invalid option.\n");
+        }
+
+    } while (choice != 5);
+    return 0;
 
 }
